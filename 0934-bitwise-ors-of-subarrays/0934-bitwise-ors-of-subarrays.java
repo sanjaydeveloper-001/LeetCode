51e224d9f9b3fd24d4865c4fee20a47f77fcf450
Sanjay D <josephstudent001@gmail.com>
2025-07-31 21:24:56 +0530

    Time: 118 ms (99.59%), Space: 71 MB (90.57%) - LeetHub

--- 0934-bitwise-ors-of-subarrays/0934-bitwise-ors-of-subarrays.java
+++ 0934-bitwise-ors-of-subarrays/0934-bitwise-ors-of-subarrays.java
@@ -1,24 +1,15 @@
-import java.util.HashSet;
-import java.util.Set;
-
+import java.util.*;
 class Solution {
     public int subarrayBitwiseORs(int[] arr) {
-       
-        Set<Integer> resultOrs = new HashSet<>();
-        
-        Set<Integer> currentOrs = new HashSet<>();
-
-        for (int x : arr) {
-            Set<Integer> nextOrs = new HashSet<>();
-            nextOrs.add(x);
-
-            for (int y : currentOrs) {
-                nextOrs.add(x | y);
+        Set<Integer> result=new HashSet<>();
+        for(int i=0;i<arr.length;i++){
+            result.add(arr[i]);
+            for(int j=i-1;j>=0;j--){
+                if(arr[j]==(arr[j] | arr[i])) break;
+                arr[j]|=arr[i];
+                result.add(arr[j]);
             }
-            resultOrs.addAll(nextOrs);
-            currentOrs = nextOrs;
         }
-
-        return resultOrs.size();
+        return result.size();
     }
 }
