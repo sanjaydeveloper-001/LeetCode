7f05d4dd080896232d94660098b5751257ba53d7
Sanjay D <josephstudent001@gmail.com>
2025-08-18 18:27:52 +0530

    Time: 72 ms (100%), Space: 68.4 MB (99.58%) - LeetHub

--- 1995-finding-pairs-with-a-certain-sum/1995-finding-pairs-with-a-certain-sum.java
+++ 1995-finding-pairs-with-a-certain-sum/1995-finding-pairs-with-a-certain-sum.java
@@ -1,32 +1,42 @@
 class FindSumPairs {
     private int[] nums1;
     private int[] nums2;
-    private Map<Integer, Integer> freq2;
+    private int[] freq;  // Frequency count of nums2 values in range [0, 1000]
 
     public FindSumPairs(int[] nums1, int[] nums2) {
         this.nums1 = nums1;
         this.nums2 = nums2;
-        freq2 = new HashMap<>();
+        this.freq = new int[999999]; // Since 0 <= nums2[i] <= 1000
+
         for (int num : nums2) {
-            freq2.put(num, freq2.getOrDefault(num, 0) + 1);
+            freq[num]++;
         }
     }
 
     public void add(int index, int val) {
         int oldVal = nums2[index];
-        freq2.put(oldVal, freq2.get(oldVal) - 1);
+        int newVal = oldVal + val;
 
-        nums2[index] += val;
-        int newVal = nums2[index];
-        freq2.put(newVal, freq2.getOrDefault(newVal, 0) + 1);
+        // Update frequency map
+        if (oldVal >= 0 && oldVal <= 999999) {
+            freq[oldVal]--;
+        }
+        if (newVal >= 0 && newVal <= 999999) {
+            freq[newVal]++;
+        }
+
+        // Update the value in nums2
+        nums2[index] = newVal;
     }
 
     public int count(int tot) {
-        int res = 0;
-        for (int a : nums1) {
-            int b = tot - a;
-            res += freq2.getOrDefault(b, 0);
+        int count = 0;
+        for (int n : nums1) {
+            int target = tot - n;
+            if (target >= 0 && target <= 999999) {
+                count += freq[target];
+            }
         }
-        return res;
+        return count;
     }
 }
