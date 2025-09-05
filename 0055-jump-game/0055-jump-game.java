b9157611db41d66076a83aaca2470f23e6448fe3
Sanjay D <josephstudent001@gmail.com>
2025-09-05 19:34:10 +0530

    Time: 3 ms (44.4%), Space: 46.3 MB (8.15%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int stepsLeft = nums[0];
+
+        for(int i=1; i<n; i++){
+            stepsLeft--;
+            if( stepsLeft < 0 ) return false;
+            if( i == n-1 ) return true;
+            if(stepsLeft < nums[i]) stepsLeft = nums[i];       
+        }
+
+        return true;        
+    }
+}
