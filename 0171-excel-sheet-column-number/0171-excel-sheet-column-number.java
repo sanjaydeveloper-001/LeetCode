28c47121ca144272301e7ee768c8cc61a6d1c645
Sanjay D <josephstudent001@gmail.com>
2025-09-12 09:58:36 +0530

    Time: 0 ms (100%), Space: 42.4 MB (29.33%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
@@ -4,10 +4,6 @@ class Solution {
         for(int i=0;i<columnTitle.length();i++){
             int num=columnTitle.charAt(i)-'A'+1;
           count=count*26+num;
-        //   System.out.println(count);
-        //   if(i+1<columnTitle.length()){
-        //     count*=26;
-        //   }
         }
         return count;
     }
