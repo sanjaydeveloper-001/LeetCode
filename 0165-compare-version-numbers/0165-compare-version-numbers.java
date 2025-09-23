87d2f821f800ccadf46c0b3ab8c9f862708e054e
Sanjay D <josephstudent001@gmail.com>
2025-09-23 20:21:23 +0530

    Time: 0 ms (100%), Space: 41.7 MB (48.13%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -1,15 +1,29 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
-        String[] v1 = version1.split("\\.");
-        String[] v2 = version2.split("\\.");
+        int i = 0, j = 0;
+        int n = version1.length(), m = version2.length();
 
-        int len = Math.max(v1.length, v2.length);
-        for (int i = 0; i < len; i++) {
-            int num1 = i < v1.length ? Integer.parseInt(v1[i]) : 0;
-            int num2 = i < v2.length ? Integer.parseInt(v2[i]) : 0;
-            if (num1 < num2) return -1;
+        while (i < n || j < m) {
+            long num1 = 0, num2 = 0; // long to avoid overflow
+
+            while (i < n && version1.charAt(i) != '.') {
+                num1 = num1 * 10 + (version1.charAt(i) - '0');
+                i++;
+            }
+
+            while (j < m && version2.charAt(j) != '.') {
+                num2 = num2 * 10 + (version2.charAt(j) - '0');
+                j++;
+            }
+
             if (num1 > num2) return 1;
+            if (num1 < num2) return -1;
+
+            // skip dots
+            if (i < n && version1.charAt(i) == '.') i++;
+            if (j < m && version2.charAt(j) == '.') j++;
         }
+
         return 0;
     }
 }
