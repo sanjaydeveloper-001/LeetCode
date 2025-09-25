c0e469bf6fee3b26d7b2a3504c6e21d37c7eb2cb
Sanjay D <josephstudent001@gmail.com>
2025-09-25 23:59:41 +0530

    Time: 2 ms (96.6%), Space: 44 MB (92.71%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,28 +1,30 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        int n = s.length();
-        int m = p.length();
-        boolean[][] dp = new boolean[n + 1][m + 1];
-        dp[n][m] = true;
-
-        for(int j = m - 1; j >= 0; j--){
-            if(p.charAt(j) == '*') dp[n][j] = dp[n][j + 1];
-        }
-
-        for(int i = n - 1; i >= 0; i--){
-            for(int j = m - 1; j >= 0; j--){
-                if(p.charAt(j) == '*'){
-                    boolean noPick = dp[i][j + 1];
-                    boolean pick = dp[i + 1][j];
-                    dp[i][j] = pick || noPick;
-                }
-                else{
-                    if(p.charAt(j) == '?' || s.charAt(i) == p.charAt(j)){
-                        dp[i][j] = dp[i + 1][j + 1];
-                    }
-                }
+        int i=0,j=0; //pointers for s and p
+        int iidx=0,staridx=-1; // last pos of * in s and corresponding i in s
+        while(i<s.length()){
+            if(j<p.length() && (p.charAt(j)=='?' || p.charAt(j)==s.charAt(i))){
+                i++;
+                j++;
             }
+            else if(j<p.length() && p.charAt(j)=='*'){
+                staridx=j;     //record pos of *
+                iidx=i;       //record i at this point
+                j++;          //move p ahead to try matching next
+            }
+            else if(staridx!=-1){
+                j=staridx+1;     //backtrack: try consume one more char with *
+                iidx++;
+                i=iidx;
+            }
+            else{
+                return false;
+            }
+        } 
+        while(j<p.length() && p.charAt(j)=='*') {
+            j++;
         }
-        return dp[0][0];
+
+        return j==p.length();
     }
 }
