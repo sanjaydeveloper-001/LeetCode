2e59989f31919bbce8eb336c1df3663e303fae1b
Sanjay D <josephstudent001@gmail.com>
2025-09-25 23:54:55 +0530

    Time: 0 ms (100%), Space: 45.8 MB (12.73%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,11 +1,23 @@
+
+
 class Solution {
-    public int minimumTotal(List<List<Integer>> tri) {
-        for (int i = tri.size() - 2; i >= 0; i--)
-            for (int j = 0; j < tri.get(i).size(); j++)
-                tri.get(i).set(j, tri.get(i).get(j) + Math.min(
-                    tri.get(i + 1).get(j),
-                    tri.get(i + 1).get(j + 1)
-                ));
-        return tri.get(0).get(0);
+    int m;
+    Integer[][] dp;
+    public int minimumTotal(List<List<Integer>> tran) {
+        m = tran.size();
+        dp = new Integer[m][m];
+       
+        return helper(tran,0,0);
+    }
+    int helper(List<List<Integer>> tran,int row, int col){
+        if(row==m){
+            return 0;
+        }
+        if(dp[row][col]!=null){
+            return dp[row][col];
+        }
+        int left = helper(tran,row+1,col);
+        int right = helper(tran,row+1,col+1);
+        return dp[row][col]=tran.get(row).get(col) + Math.min(left,right);
     }
 }
