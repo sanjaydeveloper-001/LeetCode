d5eaa6fc893b4b9938ea59ad081fe387bbc1279b
Sanjay D <josephstudent001@gmail.com>
2025-09-26 22:19:16 +0530

    Time: 22 ms (100%), Space: 56.8 MB (19.85%) - LeetHub

--- 2180-maximum-number-of-tasks-you-can-assign/2180-maximum-number-of-tasks-you-can-assign.java
+++ 2180-maximum-number-of-tasks-you-can-assign/2180-maximum-number-of-tasks-you-can-assign.java
@@ -1,44 +1,74 @@
+import java.util.*;
+
 class Solution {
-    public int maxTaskAssign(int[] tasks, int[] workers, int pills, int strength) {
-        int left = 0, right = Math.min(tasks.length, workers.length);
-        Arrays.sort(tasks);
-        Arrays.sort(workers);
-
-        while (left < right) {
-            int mid = (left + right + 1) / 2;
-            int usedPills = 0;
-            TreeMap<Integer, Integer> avail = new TreeMap<>();
-            for (int i = workers.length - mid; i < workers.length; ++i)
-                avail.put(workers[i], avail.getOrDefault(workers[i], 0) + 1);
-
-            boolean canAssign = true;
-            for (int i = mid - 1; i >= 0; --i) {
-                int t = tasks[i];
-                int w = avail.lastKey();
-                if (w >= t) {
-                    decrement(avail, w);
-                } else {
-                    Integer key = avail.ceilingKey(t - strength);
-                    if (key == null || ++usedPills > pills) {
-                        canAssign = false;
-                        break;
-                    }
-                    decrement(avail, key);
-                }
+    static final int N = 256;
+    static final int MASK = 255;
+    static final int BSHIFT = 8;
+    int[] queue = new int[50000];
+    int front = 0, back = 0;
+    void radixSort(int[] nums) {
+        int n = nums.length;
+        int[] output = new int[n];
+        int max = Arrays.stream(nums).max().getAsInt();
+        int rounds = Math.max(1, (31 - Integer.numberOfLeadingZeros(max) + BSHIFT) / BSHIFT);
+        int[] freq = new int[N];
+        int[] in = nums;
+        int[] out = output;
+        boolean swapped = false;
+        for (int round = 0; round < rounds; round++) {
+            int shift = round * BSHIFT;
+            Arrays.fill(freq, 0);
+            for (int num : in) freq[(num >> shift) & MASK]++;
+            for (int i = 1; i < N; i++) freq[i] += freq[i - 1];
+            for (int i = n - 1; i >= 0; i--) {
+                int val = in[i];
+                int x = (val >> shift) & MASK;
+                out[--freq[x]] = val;
             }
-
-            if (canAssign)
-                left = mid;
-            else
-                right = mid - 1;
+            int[] temp = in;
+            in = out;
+            out = temp;
+            swapped = !swapped;
         }
-
-        return left;
+        if (swapped)
+            System.arraycopy(in, 0, nums, 0, n);
+    }
+    int numTasks, numWorkers, pillBoost;
+    boolean isFeasible(int[] tasks, int[] workers, int k, int pills) {
+        int taskPtr = 0;
+        front = back = 0;
+        for (int i = numWorkers - k; i < numWorkers; i++) {
+            int workerStrength = workers[i];
+            while (taskPtr < k && tasks[taskPtr] <= workerStrength + pillBoost) {
+                queue[back++] = tasks[taskPtr++];
+            }
+            if (front == back) return false;
+            if (queue[front] <= workerStrength) {
+                front++;
+            } else {
+                if (pills == 0) return false;
+                pills--;
+                back--; 
+            }
+        }
+        return true;
     }
 
-    private void decrement(TreeMap<Integer, Integer> m, int k) {
-        int c = m.get(k);
-        if (c == 1) m.remove(k);
-        else m.put(k, c - 1);
+    public int maxTaskAssign(int[] tasks, int[] workers, int pills, int strength) {
+        numTasks = tasks.length;
+        numWorkers = workers.length;
+        pillBoost = strength;
+        radixSort(tasks);
+        radixSort(workers);
+        int low = 0, high = Math.min(numTasks, numWorkers);
+        while (low < high) {
+            int mid = (low + high + 1) / 2;
+            if (isFeasible(tasks, workers, mid, pills)) {
+                low = mid;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
     }
 }
