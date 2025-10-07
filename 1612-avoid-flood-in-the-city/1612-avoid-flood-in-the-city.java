1c20e9f76ad864b8b841a898036eee6d0d6f2830
Sanjay D <josephstudent001@gmail.com>
2025-10-07 08:25:35 +0530

    Time: 21 ms (100%), Space: 62.7 MB (29.22%) - LeetHub

--- 1612-avoid-flood-in-the-city/1612-avoid-flood-in-the-city.java
+++ 1612-avoid-flood-in-the-city/1612-avoid-flood-in-the-city.java
@@ -7,27 +7,25 @@ class Solution {
         }
 
         int[] ans = new int[n];
-        Map<Integer, Integer> fullDay = new HashMap<>(); // lake -> 装满日
+        Map<Integer, Integer> fullDay = new HashMap<>();
         for (int i = 0; i < n; i++) {
             int lake = rains[i];
             if (lake == 0) {
-                ans[i] = 1; // 先随便选一个湖抽干
+                ans[i] = 1;
                 continue;
             }
             Integer j = fullDay.get(lake);
             if (j != null) {
-                // 必须在 j 之后，i 之前把 lake 抽干
-                // 选一个最早的未被使用的抽水日，如果选晚的，可能会导致其他湖没有可用的抽水日
                 int dryDay = find(j + 1, fa);
                 if (dryDay >= i) {
-                    return new int[]{}; // 无法阻止洪水
+                    return new int[]{};
                 }
                 ans[dryDay] = lake;
-                fa[dryDay] = find(dryDay + 1, fa); // 删除 dryDay
+                fa[dryDay] = find(dryDay + 1, fa); 
             }
             ans[i] = -1;
-            fa[i] = i + 1; // 删除 i
-            fullDay.put(lake, i); // 插入或更新装满日
+            fa[i] = i + 1;
+            fullDay.put(lake, i);
         }
         return ans;
     }
