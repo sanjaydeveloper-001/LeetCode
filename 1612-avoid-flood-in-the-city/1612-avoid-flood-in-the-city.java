e31620d176a238c6c51d0d04e074ae58950d27a2
Sanjay D <josephstudent001@gmail.com>
2025-10-07 08:25:05 +0530

    Time: 21 ms (100%), Space: 63.5 MB (19.18%) - LeetHub

--- 1612-avoid-flood-in-the-city/1612-avoid-flood-in-the-city.java
+++ 1612-avoid-flood-in-the-city/1612-avoid-flood-in-the-city.java
@@ -1,27 +1,41 @@
 class Solution {
     public int[] avoidFlood(int[] rains) {
         int n = rains.length;
+        int[] fa = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            fa[i] = i;
+        }
+
         int[] ans = new int[n];
-        Arrays.fill(ans,-1);
-        TreeSet<Integer> set = new TreeSet<>();
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i=0;i<n;i++) {
-            if(rains[i] == 0) {
-                set.add(i);
-                ans[i] = 1;
-            } else {
-                if(map.containsKey(rains[i])) {
-                    int lastRain = map.get(rains[i]);
-                    Integer free = set.higher(lastRain);
-                    if(free == null) return new int[] {};
-                    else {
-                        ans[free] = rains[i];
-                        set.remove(free);
-                    }
-                }
-                map.put(rains[i],i);
+        Map<Integer, Integer> fullDay = new HashMap<>(); // lake -> 装满日
+        for (int i = 0; i < n; i++) {
+            int lake = rains[i];
+            if (lake == 0) {
+                ans[i] = 1; // 先随便选一个湖抽干
+                continue;
             }
+            Integer j = fullDay.get(lake);
+            if (j != null) {
+                // 必须在 j 之后，i 之前把 lake 抽干
+                // 选一个最早的未被使用的抽水日，如果选晚的，可能会导致其他湖没有可用的抽水日
+                int dryDay = find(j + 1, fa);
+                if (dryDay >= i) {
+                    return new int[]{}; // 无法阻止洪水
+                }
+                ans[dryDay] = lake;
+                fa[dryDay] = find(dryDay + 1, fa); // 删除 dryDay
+            }
+            ans[i] = -1;
+            fa[i] = i + 1; // 删除 i
+            fullDay.put(lake, i); // 插入或更新装满日
         }
         return ans;
+    }
+
+    private int find(int x, int[] fa) {
+        if (fa[x] != x) {
+            fa[x] = find(fa[x], fa);
+        }
+        return fa[x];
     }
 }
