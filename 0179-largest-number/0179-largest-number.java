108853c9bd57b149f85d8ae06ccfa32b49abd2d1
Sanjay D <josephstudent001@gmail.com>
2025-10-10 23:38:04 +0530

    Time: 1 ms (100%), Space: 41.7 MB (99.62%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -1,17 +1,52 @@
 class Solution {
     public String largestNumber(int[] nums) {
-        String[] a = new String[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            a[i] = String.valueOf(nums[i]);
+        quickSort(nums, 0, nums.length - 1);
+        if (nums[0] == 0) return "0";
+        StringBuilder sb = new StringBuilder();
+        for (int num : nums) {
+            sb.append(num);
         }
-        Arrays.sort(a, (x, y) -> (y + x).compareTo(x + y));
-        StringBuilder s = new StringBuilder();
-        for (String x : a) {
-            s.append(x);
+        return sb.toString();
+    }
+
+    private void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
         }
-        while (s.length() > 1 && s.charAt(0) == '0') {
-            s.deleteCharAt(0);
+    }
+
+    private int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (compare(arr[j], pivot) > 0) {
+                i++;
+                swap(arr, i, j);
+            }
         }
-        return s.toString();
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    private int compare(int x, int y) {
+        long xy = concat(x, y);
+        long yx = concat(y, x);
+        return Long.compare(xy, yx);
+    }
+
+    private long concat(int a, int b) {
+        int pow = 10;
+        while (pow <= b) {
+            pow *= 10;
+        }
+        return (long) a * pow + b;
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
 }
