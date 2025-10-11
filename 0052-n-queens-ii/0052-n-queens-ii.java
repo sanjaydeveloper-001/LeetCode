123dde227957ff158aac656671a0ee90507331e5
Sanjay D <josephstudent001@gmail.com>
2025-10-11 12:02:34 +0530

    Time: 0 ms (100%), Space: 40.4 MB (72.29%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.java
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -1,31 +1,21 @@
 class Solution {
-    int count = 0;
-    int[] arr;
-
     public int totalNQueens(int n) {
-        arr = new int[n];
-        helper(0, n);
-        return count;
+        return solve(0, 0, 0, 0, n);
     }
+    
+    private int solve(int row, int cols, int diags, int antiDiags, int n) {
+        if (row == n)
+            return 1;
 
-    private void helper(int row, int n) {
-        if (row == n) {
-            count++;
-            return;
-        }
-        for (int col = 0; col < n; col++) {
-            if (isSafe(row, col)) {
-                arr[row] = col;
-                helper(row + 1, n);
-            }
-        }
-    }
+        int availablePositions = ((1 << n) - 1) & ~(cols | diags | antiDiags);
 
-    private boolean isSafe(int row, int col) {
-        for (int i = 0; i < row; i++) {
-            if (arr[i] == col || Math.abs(row - i) == Math.abs(col - arr[i]))
-                return false;
+        int solutions = 0;
+        while (availablePositions != 0) {
+            int pos = availablePositions & -availablePositions;
+            availablePositions &= availablePositions - 1;
+            solutions += solve(row + 1, cols | pos, (diags | pos) << 1, (antiDiags | pos) >> 1, n);
         }
-        return true;
+
+        return solutions;
     }
 }
