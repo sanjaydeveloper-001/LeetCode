c1b85401936c6a4dee2845bf1498afd95bf12310
Sanjay D <josephstudent001@gmail.com>
2025-10-26 21:45:51 +0530

    Time: 94 ms (99.95%), Space: 101.2 MB (58.89%) - LeetHub

--- 2169-simple-bank-system/2169-simple-bank-system.java
+++ 2169-simple-bank-system/2169-simple-bank-system.java
@@ -1,32 +1,36 @@
 class Bank {
-    private final long[] bal;
-    private final int n;
 
-    public Bank(long[] bal) {
-        this.bal = bal;
-        this.n = bal.length;
+    private long[] balance;
+
+    public Bank(long[] balance) {
+        this.balance = balance;
     }
+    
+    public boolean transfer(int account1, int account2, long money) {
+        if(account1-1 >= balance.length || account2-1 >= balance.length) return false;
+            
+        if(withdraw(account1, money)) {
+            return deposit(account2, money);
+        }
 
-    public boolean transfer(int from, int to, long amt) {
-        if (!valid(from) || !valid(to) || bal[from - 1] < amt) return false;
-        bal[from - 1] -= amt;
-        bal[to - 1] += amt;
+        return false;
+    }
+    
+    public boolean deposit(int account, long money) {
+        if(account-1 < balance.length) {
+            this.balance[account-1] += money;
+            return true;
+        }
+
+        return false;
+    }
+    
+    public boolean withdraw(int account, long money) {
+        if(account-1 >= this.balance.length) return false;
+
+        if(this.balance[account-1] < money) return false;
+
+        this.balance[account-1] -= money;
         return true;
-    }
-
-    public boolean deposit(int acc, long amt) {
-        if (!valid(acc)) return false;
-        bal[acc - 1] += amt;
-        return true;
-    }
-
-    public boolean withdraw(int acc, long amt) {
-        if (!valid(acc) || bal[acc - 1] < amt) return false;
-        bal[acc - 1] -= amt;
-        return true;
-    }
-
-    private boolean valid(int acc) {
-        return acc > 0 && acc <= n;
     }
 }
