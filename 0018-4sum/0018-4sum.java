4a8748cd29977909640d1352deea796c5f2bcdc4
Sanjay D <josephstudent001@gmail.com>
2025-10-30 14:45:31 +0530

    Time: 0 ms (100%), Space: 45.9 MB (6.49%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -7,12 +7,10 @@ class Solution {
                 init();
                 return result.get(index);
             }
-
             public int size() {
                 init();
                 return result.size();
             }
-
             private void init(){ 
                 List<List<Integer>> res = new ArrayList<>();
                 Set<List<Integer>> resSet = new HashSet<>();
@@ -23,13 +21,12 @@ class Solution {
                         long newTarget = (long) target - (long) nums[i] - (long) nums[j]; 
                         int low = j+1, high = n-1;
                         while(low < high){
-
                             if(nums[low] + nums[high] == newTarget){
                                 resSet.add(Arrays.asList(nums[low] , nums[high] , nums[i] , nums[j]));
                                 while(low<high && nums[low] == nums[low+1]) low++;
                                 while(low<high && nums[high] == nums[low-1]) high--;
-                                low++;
-                                high--;
+                                    low++;
+                                    high--;
                             }
                             else if(nums[low] + nums[high] < newTarget){
                                 low++;
