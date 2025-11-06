5e52a4b05da798e1ea8ed93b31f7b8692f85aba5
Sanjay D <josephstudent001@gmail.com>
2025-11-06 10:17:04 +0530

    Time: 19 ms (99.58%), Space: 258.9 MB (6.7%) - LeetHub

--- 3863-power-grid-maintenance/3863-power-grid-maintenance.java
+++ 3863-power-grid-maintenance/3863-power-grid-maintenance.java
@@ -1,73 +1,77 @@
-import java.util.*;
-
 class Solution {
-    int[] parent, size;
-    Map<Integer, PriorityQueue<Integer>> mp = new HashMap<>();
-
-    int findParent(int node) {
-        if (parent[node] == node) return node;
-        return parent[node] = findParent(parent[node]);
-    }
-
-    void Union(int u, int v) {
-        int up = findParent(u);
-        int vp = findParent(v);
-
-        if (up == vp) return;
-
-        if (size[up] > size[vp]) {
-            size[up] += size[vp];
-            parent[vp] = up;
-            PriorityQueue<Integer> pqV = mp.get(vp);
-            PriorityQueue<Integer> pqU = mp.get(up);
-            while (!pqV.isEmpty()) pqU.offer(pqV.poll());
-        } else {
-            size[vp] += size[up];
-            parent[up] = vp;
-            PriorityQueue<Integer> pqU = mp.get(up);
-            PriorityQueue<Integer> pqV = mp.get(vp);
-            while (!pqU.isEmpty()) pqV.offer(pqU.poll());
+    public int[] processQueries(int n, int[][] connections, int[][] queries) {
+        n++;
+        final int[] l = new int[n];
+        for (int i = 1; i < n; i++) {
+            l[i] = i;
         }
-    }
-
-    public int[] processQueries(int c, int[][] connections, int[][] queries) {
-        parent = new int[c];
-        size = new int[c];
-        boolean[] offline = new boolean[c];
-
-        for (int i = 0; i < c; i++) {
-            parent[i] = i;
-            size[i] = 1;
-            PriorityQueue<Integer> pq = new PriorityQueue<>();
-            pq.offer(i);
-            mp.put(i, pq);
+        for (int[] as : connections) {
+            l[getLabel(l, as[0])] = l[getLabel(l, as[1])];
         }
-
-        for (int[] conn : connections) {
-            int u = conn[0] - 1, v = conn[1] - 1;
-            Union(u, v);
+        final int[] counts = new int[n];
+        for (int i = 0; i < n; i++) {
+            counts[getLabel(l, i)]++;
         }
-
-        List<Integer> ans = new ArrayList<>();
-
-        for (int[] q : queries) {
-            int type = q[0];
-            int node = q[1] - 1;
-
-            if (type == 1) {
-                if (!offline[node]) {
-                    ans.add(node + 1);
-                    continue;
+        updateCounts(counts);
+        final int[] starts = counts.clone();
+        final int[] sorted = new int[n];
+        for (int i = 0; i < n; i++) {
+            sorted[counts[l[i]]++] = i;
+        }
+        final int[] r = new int[queries.length];
+        int len = 0;
+        final boolean[] offline = new boolean[n];
+        for (var q: queries) {
+            final int x = q[1];
+            if (q[0] == 1) {
+                if (offline[x]) {
+                    final int label = l[x];
+                    final int end = counts[label];
+                    int start = starts[label];
+                    while (start < end && offline[sorted[start]]) {
+                        start++;
+                    }
+                    starts[label] = start;
+                    r[len++] = start == end ? -1 : sorted[start];
+                } else {
+                    r[len++] = x;
                 }
-                int par = findParent(node);
-                PriorityQueue<Integer> pq = mp.get(par);
-                while (!pq.isEmpty() && offline[pq.peek()]) pq.poll();
-                ans.add(pq.isEmpty() ? -1 : pq.peek() + 1);
             } else {
-                offline[node] = true;
+                offline[x] = true;
             }
         }
+        return Arrays.copyOf(r, len);
+    }
 
-        return ans.stream().mapToInt(i -> i).toArray();
+
+    static final int[] arr = new int[100_001];
+
+    private static int check(int[] sorted, int[] source, int[] target, int start, int end) {
+        int r = 0;
+        for (int i = start; i < end; i++) {
+            final int idx = sorted[i];
+            if (++arr[source[idx]] > 0) r++;
+            if (arr[target[idx]]-- > 0) r--;
+        }
+        for (int i = start; i < end; i++) {
+            final int idx = sorted[i];
+            arr[source[idx]] = 0;
+            arr[target[idx]] = 0;
+        }
+        return r;
+    }
+
+    private static void updateCounts(int[] count) {
+        int sum = 0;
+        for (int r = 0; r < count.length; r++) {
+            final int newSum = sum + count[r];
+            count[r] = sum;
+            sum = newSum;
+        }
+    }
+
+    static int getLabel(final int[] labels, int idx) {
+        final int current = labels[idx];
+        return (current == idx || current < 0)? idx : (labels[idx] = getLabel(labels, current));
     }
 }
