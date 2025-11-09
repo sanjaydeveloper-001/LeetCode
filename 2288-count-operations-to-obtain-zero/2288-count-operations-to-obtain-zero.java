2ae5a7038b80aadcf4dd9460f5e956ed38e26137
Sanjay D <josephstudent001@gmail.com>
2025-11-09 21:16:13 +0530

    Time: 0 ms (100%), Space: 42 MB (20.29%) - LeetHub

--- 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.java
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.java
@@ -1,9 +1,17 @@
-class Solution {
-    public int countOperations(int num1, int num2) {
-        if(num1 <= 0 || num2 <= 0) return 0;
-        if(num1 >= num2){
-            return 1 + countOperations(num1-num2, num2);
+class Solution 
+{
+    public int countOperations(int num1, int num2) 
+    {
+        int res = 0;
+        while (num1 != 0 && num2 != 0) 
+        {
+            res += num1 / num2;
+            num1 %= num2;
+            
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
         }
-        return 1 + countOperations(num1, num2-num1);
+        return res;
     }
 }
