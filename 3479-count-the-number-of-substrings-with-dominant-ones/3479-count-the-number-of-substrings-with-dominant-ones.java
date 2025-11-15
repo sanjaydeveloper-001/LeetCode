42971f1c7a550dccf67b07063841c0352cb66bf1
Sanjay D <josephstudent001@gmail.com>
2025-11-15 20:45:26 +0530

    Time: 57 ms (91.59%), Space: 47.2 MB (24.3%) - LeetHub

--- 3479-count-the-number-of-substrings-with-dominant-ones/3479-count-the-number-of-substrings-with-dominant-ones.java
+++ 3479-count-the-number-of-substrings-with-dominant-ones/3479-count-the-number-of-substrings-with-dominant-ones.java
@@ -1,51 +1,46 @@
 class Solution {
-    public int numberOfSubstrings(String s) {
-        int n = s.length();
-        int[] pref = new int[n + 1];
-        for (int i = 0; i < n; i++)
-            pref[i + 1] = pref[i] + (s.charAt(i) == '1' ? 1 : 0);
+  public int numberOfSubstrings(String s) {
+    return prefixEnumeration(s);
+  }
 
-        List<Integer> Z = new ArrayList<>();
-        for (int i = 0; i < n; i++)
-            if (s.charAt(i) == '0') Z.add(i);
-        int m = Z.size();
+  private int prefixEnumeration(String s) {
+    int n = s.length();
 
-        long ans = 0;  
+    int[] zerosIdx = new int[n + 1];
+    int nextZeroIdx = 1;
+    zerosIdx[0] = -1;
 
-        int i = 0;
-        while (i < n) {
-            if (s.charAt(i) == '0') { i++; continue; }
-            int j = i;
-            while (j < n && s.charAt(j) == '1') j++;
-            long len = j - i;
-            ans += len * (len + 1) / 2;
-            i = j;
-        }
+    int totalOnes = 0;
 
-        int B = (int)Math.sqrt(n) + 2;
+    int res = 0;
 
-        for (int a = 0; a < m; a++) {
-            int Lmin = (a == 0 ? 0 : Z.get(a - 1) + 1), Lmax = Z.get(a);
-            if (Lmin > Lmax) continue;
+    for(int right = 0; right < n; right++) {
+      if(s.charAt(right) == '0') {
+        zerosIdx[nextZeroIdx++] = right;
+      } else {
+        res += right - zerosIdx[nextZeroIdx - 1];
+        totalOnes++;
+      }
 
-            for (int z = 1; z <= B; z++) {
-                int b = a + z - 1;
-                if (b >= m) break;
+      for(int zeroPos = nextZeroIdx - 1; zeroPos > 0 && (nextZeroIdx - zeroPos) * (nextZeroIdx - zeroPos) <= totalOnes; zeroPos--) {
+        int zerosCount = nextZeroIdx - zeroPos; //nextZeroIdx - 1 (zeros at right pos) - zeroes at earlier pos + 1
 
-                int Rmin = Z.get(b), Rmax = (b + 1 < m ? Z.get(b + 1) - 1 : n - 1);
-                if (Rmin > Rmax) continue;
+        int onesCount = right - zerosIdx[zeroPos] + 1 - zerosCount; //totalLength - zerosCount
 
-                int need = z * z, r = Rmin;
+        int onesDeficit = zerosCount * zerosCount - onesCount; //how many ones we're missing within [zeroIdx[z - 1]+1, zeroIdx[z]] range to satisfy
+                                                                //the condition - therefore, how many substrings starting farther to the right
+                                                                //(closer to zPos) would not satisfy it
 
-                for (int l = Lmin; l <= Lmax; l++) {
-                    if (pref[Rmax + 1] - pref[l] < need) continue;
-                    while (r <= Rmax && pref[r + 1] - pref[l] < need) r++;
-                    if (r > Rmax) break;
-                    ans += (Rmax - r + 1);
-                }
-            }
-        }
+        int extendable = zerosIdx[zeroPos] - zerosIdx[zeroPos - 1]; //how far back are we looking now - essentially, how many positions can we 
+                                                                    //start our substring from
 
-        return (int) ans;  
+        res += Math.max(extendable - Math.max(onesDeficit, 0), 0); //all the possible substrings (extendable) decreased by the number of 
+                                                                    //invalid substrings (those starting further to the right, and therefore
+                                                                    // having less ones)
+
+      }
     }
+
+    return res;
+  }
 }
