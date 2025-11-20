b07d68cb0d2b9a9f155d7aad84b300a75239f05f
Sanjay D <josephstudent001@gmail.com>
2025-11-20 12:48:13 +0530

    Time: 9 ms (93.2%), Space: 47.8 MB (11.56%) - LeetHub

--- 0759-set-intersection-size-at-least-two/0759-set-intersection-size-at-least-two.java
+++ 0759-set-intersection-size-at-least-two/0759-set-intersection-size-at-least-two.java
@@ -1,34 +1,33 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int intersectionSizeTwo(int[][] intervals) {
-        int n = 0;
-        long[] endStartPairs = new long[intervals.length];
+        // Sort first by end ascending, then start descending
+        Arrays.sort(intervals, (a, b) -> 
+            a[1] != b[1] ? Integer.compare(a[1], b[1]) : Integer.compare(b[0], a[0]));
+        
+        int ans = 0;
+        int a = -1, b = -1;
         for (int[] interval : intervals) {
-            endStartPairs[n] = -interval[0] & 0xFFFFFFFFL;
-            endStartPairs[n++] |= (long) (interval[1]) << 32;
-        }
-        Arrays.sort(endStartPairs);
-        int min = -2;
-        int max = -1;
-        int curStart;
-        int curEnd;
-        int res = 0;
-        for (long endStartPair : endStartPairs) {
-            curStart = -(int) endStartPair;
-            curEnd = (int) (endStartPair >> 32);
-            if (curStart <= min) {
-                continue;
-            }
-            if (curStart <= max) {
-                res += 1;
-                min = max;
+            int l = interval[0], r = interval[1];
+            // a and b are most recently added elements, always a < b
+            boolean inA = (a >= l && a <= r);
+            boolean inB = (b >= l && b <= r);
+            
+            if (inA && inB) continue;
+            // Need to add two elements
+            if (!inA && !inB) {
+                ans += 2;
+                a = r - 1;
+                b = r;
             } else {
-                res += 2;
-                min = curEnd - 1;
+                // Only one is covered, add the largest
+                ans += 1;
+                // a is always the smaller, so b is the one that's within interval
+                a = b;
+                b = r;
             }
-            max = curEnd;
         }
-        return res;
+        return ans;
     }
 }
