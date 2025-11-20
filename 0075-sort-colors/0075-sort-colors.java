14faf58e061e8b21ac236f3c484ebabe6c2a9e6d
Sanjay D <josephstudent001@gmail.com>
2025-11-20 12:51:32 +0530

    Time: 0 ms (100%), Space: 43.6 MB (9.59%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,18 +1,45 @@
 class Solution {
     public void sortColors(int[] nums) {
-    int low = 0, mid = 0, high = nums.length - 1;
-    while (mid <= high) {
-        if (nums[mid] == 0) {
-            int tmp = nums[low];
-            nums[low++] = nums[mid];
-            nums[mid++] = tmp;
-        } else if (nums[mid] == 1) {
-            mid++;
-        } else {
-            int tmp = nums[mid];
-            nums[mid] = nums[high];
-            nums[high--] = tmp;
+        if (nums.length <= 1) {
+            System.out.println(Arrays.toString(nums));
+            return;
         }
+        int i = 0;
+        int j = nums.length - 1;
+        int arr[] = new int[nums.length];
+        int left = 0;
+        int right = nums.length - 1;
+        int zeroCount = 0;
+        int oneCount = 0;
+        while (i <= j) {
+            if (nums[i] == 0) {
+                arr[left] = 0;
+                left++;
+                zeroCount++;
+            } else if (nums[i] == 2) {
+                arr[right] = 2;
+                right--;
+            } else {
+                oneCount++;
+            }
+            i++;
+            if (i < j) {
+                if (nums[j] == 0) {
+                    arr[left] = 0;
+                    left++;
+                    zeroCount++;
+                } else if (nums[j] == 2) {
+                    arr[right] = 2;
+                    right--;
+                } else {
+                    oneCount++;
+                }
+            j--;
+            }
+        }
+        for (int k = zeroCount; k < zeroCount + oneCount; k++) {
+            arr[k] = 1;
+        }
+        System.arraycopy(arr, 0, nums, 0, nums.length);
     }
-}
 }
