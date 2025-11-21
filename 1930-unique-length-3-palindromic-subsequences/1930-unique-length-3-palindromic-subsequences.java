b74eccf6adb9c0b44ef51f4259d1ec1d03db8e40
Sanjay D <josephstudent001@gmail.com>
2025-11-21 18:30:53 +0530

    Time: 16 ms (99.39%), Space: 46.9 MB (23.4%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.java
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.java
@@ -1,33 +1,37 @@
 class Solution {
     public int countPalindromicSubsequence(String s) {
-        Set<Character> letters = new HashSet();
-        for (Character c: s.toCharArray()) {
-            letters.add(c);
-        }
-        
-        int ans = 0;
-        for (Character letter : letters) {
-            int i = -1;
-            int j = 0;
-            
-            for (int k = 0; k < s.length(); k++) {
-                if (s.charAt(k) == letter) {
-                    if (i == -1) {
-                        i = k;
-                    }
-                    
-                    j = k;
-                }
+        int n = s.length();
+        int[] charFirst = new int[26];
+        int[] posBit = new int[n];
+        Arrays.fill(charFirst, -1);
+
+        int bits = 0;
+        for (int i = 0; i < n; i++) {
+            int idx = s.charAt(i) - 'a';
+            posBit[i] = bits |= 1 << idx;
+            if (charFirst[idx] == -1) {
+                charFirst[idx] = i;
+                bits = 0;
             }
-            
-            Set<Character> between = new HashSet();
-            for (int k = i + 1; k < j; k++) {
-                between.add(s.charAt(k));
-            }
-            
-            ans += between.size();
         }
-        
-        return ans;
+
+        int res = 0;
+        for (int i = n - 1; i >= 2; i--) {
+            int idx = s.charAt(i) - 'a';
+            if (charFirst[idx] <= -1 || charFirst[idx] == i) {
+                continue;
+            }
+            res += Integer.bitCount(arrOr(charFirst[idx] + 1, i - 1, posBit));
+            charFirst[idx] = -2;
+        }
+        return res;
+    }
+
+    private int arrOr(int start, int end, int[] arr) {
+        int res = 0;
+        for (int i=start; i<=end; i++) {
+            res |= arr[i];
+        }
+        return res;
     }
 }
