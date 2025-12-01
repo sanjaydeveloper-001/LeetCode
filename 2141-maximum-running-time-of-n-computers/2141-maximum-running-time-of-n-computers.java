d70b7be1b31d9838901c017d32805be2317bea8b
Sanjay D <josephstudent001@gmail.com>
2025-12-01 23:10:23 +0530

    Time: 6 ms (100%), Space: 74.1 MB (23.63%) - LeetHub

--- 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.java
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.java
@@ -1,20 +1,43 @@
 class Solution {
     public long maxRunTime(int n, int[] batteries) {
-        long totalEnergy = 0;
-        for (int battery : batteries) {
-            totalEnergy += battery;
-        }
-
-        Arrays.sort(batteries);
-
-        for (int i = batteries.length - 1; i >= 0; i--) {
-            if (batteries[i] > totalEnergy / n) {
-                totalEnergy -= batteries[i];
-                n--;
-            } else {
-                break;
+        long sum=0;
+        int max=0;
+        for(int i=0;i<batteries.length;i++){
+            sum+=batteries[i];
+            if(batteries[i]>max){
+                max=batteries[i];
             }
         }
-        return totalEnergy / n;
+        long right=sum/n;
+        if(right>=max){
+            return right;
+        }
+        long left=0;
+        long result=0;
+        while(left<=right){
+           long mid=left+(right-left)/2;
+           if(canCoverAllComputer(batteries,n,mid)){
+               result=mid;
+               left=mid+1;
+           }else{
+               right=mid-1;
+           }
+        }
+        return result;
+    }
+    
+    public boolean canCoverAllComputer(int[] batteries,int n,long time){
+        long sum=0;
+        for(int i=0;i<batteries.length;i++){
+            if(batteries[i]>=time){
+                n--;
+            }else{
+                sum+=batteries[i];
+            }
+            if(sum>=(long)n*time){
+                return true;
+            }
+        }
+        return false;
     }
 }
