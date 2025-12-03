d523a89e77c5fb4955115eba530a411accc681a2
Sanjay D <josephstudent001@gmail.com>
2025-12-03 21:45:34 +0530

    Time: 357 ms (97.66%), Space: 275 MB (70.31%) - LeetHub

--- 3625-count-number-of-trapezoids-ii/3625-count-number-of-trapezoids-ii.java
+++ 3625-count-number-of-trapezoids-ii/3625-count-number-of-trapezoids-ii.java
@@ -1,63 +1,76 @@
-import java.util.*;
-
 class Solution {
-    public int countTrapezoids(int[][] points) {
-        HashMap<Integer, HashMap<Integer, Integer>> t = new HashMap<>();
-        HashMap<Integer, HashMap<Integer, Integer>> v = new HashMap<>();
 
+    public int countTrapezoids(int[][] points) {
         int n = points.length;
+        double inf = 1e9 + 7;
+        Map<Double, List<Double>> slopeToIntercept = new HashMap<>();
+        Map<Integer, List<Double>> midToSlope = new HashMap<>();
+        int ans = 0;
 
         for (int i = 0; i < n; i++) {
+            int x1 = points[i][0];
+            int y1 = points[i][1];
             for (int j = i + 1; j < n; j++) {
+                int x2 = points[j][0];
+                int y2 = points[j][1];
+                int dx = x1 - x2;
+                int dy = y1 - y2;
+                double k;
+                double b;
 
-                int dx = points[j][0] - points[i][0];
-                int dy = points[j][1] - points[i][1];
-
-                if (dx < 0 || (dx == 0 && dy < 0)) {
-                    dx = -dx;
-                    dy = -dy;
+                if (x2 == x1) {
+                    k = inf;
+                    b = x1;
+                } else {
+                    k = (1.0 * (y2 - y1)) / (x2 - x1);
+                    b = (1.0 * (y1 * dx - x1 * dy)) / dx;
                 }
-
-                int g = gcd(dx, Math.abs(dy));
-                int sx = dx / g;
-                int sy = dy / g;
-
-                int des = sx * points[i][1] - sy * points[i][0];
-
-                int key1 = (sx << 12) | (sy + 2000);
-                int key2 = (dx << 12) | (dy + 2000);
-
-                t.computeIfAbsent(key1, k -> new HashMap<>()).merge(des, 1, Integer::sum);
-                v.computeIfAbsent(key2, k -> new HashMap<>()).merge(des, 1, Integer::sum);
+                if (k == -0.0) {
+                    k = 0.0;
+                }
+                if (b == -0.0) {
+                    b = 0.0;
+                }
+                int mid = (x1 + x2) * 10000 + (y1 + y2);
+                slopeToIntercept
+                    .computeIfAbsent(k, key -> new ArrayList<>())
+                    .add(b);
+                midToSlope
+                    .computeIfAbsent(mid, key -> new ArrayList<>())
+                    .add(k);
             }
         }
 
-        return count(t) - count(v) / 2;
-    }
-
-    private int count(HashMap<Integer, HashMap<Integer, Integer>> map) {
-        long ans = 0;
-
-        for (HashMap<Integer, Integer> inner : map.values()) {
-            long sum = 0;
-
-            for (int val : inner.values()) sum += val;
-
-            for (int val : inner.values()) {
-                sum -= val;
-                ans += (long) val * sum;
+        for (List<Double> sti : slopeToIntercept.values()) {
+            if (sti.size() == 1) {
+                continue;
+            }
+            Map<Double, Integer> cnt = new TreeMap<>();
+            for (double b : sti) {
+                cnt.put(b, cnt.getOrDefault(b, 0) + 1);
+            }
+            int sum = 0;
+            for (int count : cnt.values()) {
+                ans += sum * count;
+                sum += count;
             }
         }
 
-        return (int) ans;
-    }
-
-    private int gcd(int a, int b) {
-        while (b != 0) {
-            int t = a % b;
-            a = b;
-            b = t;
+        for (List<Double> mts : midToSlope.values()) {
+            if (mts.size() == 1) {
+                continue;
+            }
+            Map<Double, Integer> cnt = new TreeMap<>();
+            for (double k : mts) {
+                cnt.put(k, cnt.getOrDefault(k, 0) + 1);
+            }
+            int sum = 0;
+            for (int count : cnt.values()) {
+                ans -= sum * count;
+                sum += count;
+            }
         }
-        return Math.abs(a);
+
+        return ans;
     }
 }
