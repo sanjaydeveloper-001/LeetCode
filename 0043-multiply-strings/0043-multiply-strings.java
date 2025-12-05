958811fd2165139657659d74505b7e06a684d44c
Sanjay D <josephstudent001@gmail.com>
2025-12-05 21:26:49 +0530

    Time: 2 ms (98.82%), Space: 43.6 MB (31.76%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.java
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -1,9 +1,25 @@
 class Solution {
     public String multiply(String num1, String num2) {
-        
-        long s = Integer.parseInt(num1);
-        long t = Integer.parseInt(num2);
-        long mul = s*t;
-        return String.valueOf(mul);
+        int[] steps = new int[num1.length() + num2.length()];
+
+        for (int i = num1.length() - 1; i >= 0; i--) {
+            int d1 = num1.charAt(i) - '0';
+            for (int j = num2.length() - 1; j >= 0; j--) {
+                steps[i + j + 1] += d1 * (num2.charAt(j) - '0');
+            }
+        }
+        for (int i = steps.length - 1; i > 0; i--) {
+            steps[i - 1] += steps[i] / 10;
+            steps[i] %= 10;
+        }
+        int i = 0;
+        while (i < steps.length - 1 && steps[i] == 0) {
+            i++;
+        }
+        StringBuilder sb = new StringBuilder(i + 1);
+        for (; i < steps.length; i++) {
+            sb.append((char)(steps[i] + '0'));
+        }
+        return sb.toString();
     }
 }
