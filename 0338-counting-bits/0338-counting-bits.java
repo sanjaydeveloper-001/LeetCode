35d002ac97b2f7650cad8f9e0f99e33eb6f85c93
Sanjay D <josephstudent001@gmail.com>
2025-12-08 10:08:22 +0530

    Time: 0 ms (100%), Space: 48.6 MB (36.34%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,25 +1,18 @@
+// Leetcode Best Recursive solution
 class Solution {
+
+    private void populate(int[] ans, int i, int val){
+        if(i >= ans.length) return;
+        ans[i] = val;
+        populate(ans, i * 2, val);
+        populate(ans, i * 2 + 1, val + 1);
+    }
+
     public int[] countBits(int n) {
-        int [] arr = new int[n+1];
-        int i = 0;
-        while(i<=n){
-            if( i < 8) {
-                switch(i){
-                    case 0 : { arr[i] = 0; break;}
-                    case 1 : { arr[i] = 1; break;}
-                    case 2 : { arr[i] = 1; break;}
-                    case 3 : { arr[i] = 2; break;}
-                    case 4 : { arr[i] = 1; break;}
-                    case 5 : { arr[i] = 2; break;}
-                    case 6 : { arr[i] = 2; break;}
-                    case 7 : { arr[i] = 3; break;}
-                }
-            }
-            else{
-                arr[i] = arr[i/2] + arr[i%2];
-            }
-            i++;
-        }
-        return arr;
+        int[] ans = new int[n + 1];
+        ans[0] = 0;
+        populate(ans, 1, 1);
+
+        return ans;  
     }
 }
