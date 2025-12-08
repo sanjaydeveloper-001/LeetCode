35dc88ac3a68e003d15aafc14621641039307610
Sanjay D <josephstudent001@gmail.com>
2025-12-08 08:43:04 +0530

    Time: 0 ms (100%), Space: 42.2 MB (26.3%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.java
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.java
@@ -1,13 +1,22 @@
 class Solution {
     public int countTriples(int n) {
-        int c = 0;
-        for(int i=1; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                int sum = (i*i + j*j);
-                int sqt = (int)Math.sqrt(sum);
-                if(sqt * sqt == sum && sqt <= n)  c+=2;
+        int ans = 0;
+        for (int u = 3; u * u < n * 2; u += 2) {
+            for (int v = 1; v < u && u * u + v * v <= n * 2; v += 2) {
+                if (gcd(u, v) == 1) {
+                    ans += n * 2 / (u * u + v * v);
+                }
             }
         }
-        return c;
+        return ans * 2; // (a,b,c) 和 (b,a,c) 各算一次
+    }
+
+    private int gcd(int a, int b) {
+        while (a != 0) {
+            int tmp = a;
+            a = b % a;
+            b = tmp;
+        }
+        return b;
     }
 }
