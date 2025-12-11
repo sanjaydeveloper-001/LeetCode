144e56e0b60193c3fca41c6beeb0602c7b9624b4
Sanjay D <josephstudent001@gmail.com>
2025-12-11 17:52:00 +0530

    Time: 1 ms (63.95%), Space: 42.1 MB (78.58%) - LeetHub

---
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int pivotInteger(int n) {
+        int left = 0;
+        int right = 0;
+        for(int i=1; i<=n; i++) right += i;
+        for(int i=1; i<=n; i++){
+            left += i;
+            if(left == right) return i;
+            right -= i;
+        }
+        return -1;
+    }
+}
