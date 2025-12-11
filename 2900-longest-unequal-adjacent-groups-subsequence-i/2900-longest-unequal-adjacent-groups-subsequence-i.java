297bcf621d5f9e93fa2727ae7b3e510e2e48fb05
Sanjay D <josephstudent001@gmail.com>
2025-12-11 08:34:34 +0530

    Time: 1 ms (95.42%), Space: 46.6 MB (41.6%) - LeetHub

--- 2900-longest-unequal-adjacent-groups-subsequence-i/2900-longest-unequal-adjacent-groups-subsequence-i.java
+++ 2900-longest-unequal-adjacent-groups-subsequence-i/2900-longest-unequal-adjacent-groups-subsequence-i.java
@@ -10,4 +10,45 @@ class Solution {
         }
         return result;
     }
+
+    // public List<String> getLongestSubsequence(String[] words, int[] groups) {
+    //     int n = words.length;
+    //     int[] length = new int[n];
+    //     int[] prev = new int[n];
+        
+    //     for (int i = 0; i < n; i++) {
+    //         length[i] = 1;  // каждый элемент сам по себе
+    //         prev[i] = -1;
+    //     }
+        
+    //     for (int i = 0; i < n; i++) {
+    //         for (int j = 0; j < i; j++) {
+    //             if (groups[i] != groups[j] && length[j] + 1 > length[i]) {
+    //                 length[i] = length[j] + 1;
+    //                 prev[i] = j;
+    //             }
+    //         }
+    //     }
+        
+    //     int maxIndex = 0;
+    //     for (int i = 1; i < n; i++) {
+    //         if (length[i] > length[maxIndex]) {
+    //             maxIndex = i;
+    //         }
+    //     }
+        
+    //     List<String> result = new ArrayList<>();
+    //     int current = maxIndex;
+    //     Stack<String> stack = new Stack<>();
+        
+    //     while (current != -1) {
+    //         stack.push(words[current]);
+    //         current = prev[current];
+    //     }
+        
+    //     while (!stack.isEmpty()) {
+    //         result.add(stack.pop());
+    //     }        
+    //     return result;
+    // }
 }
