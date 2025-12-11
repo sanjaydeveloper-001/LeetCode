a95e250745edc026b82480d92566a5053ebf274f
Sanjay D <josephstudent001@gmail.com>
2025-12-11 17:47:46 +0530

    Time: 0 ms (100%), Space: 43.8 MB (81.33%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
@@ -1,13 +1,57 @@
 class Solution {
     public boolean canMakeArithmeticProgression(int[] arr) {
-         Arrays.sort(arr);
-        int diff = arr[1] - arr[0];
+        // Arrays.sort(arr);
+        // int diff=arr[1]-arr[0];
+        // boolean ans;
+        // for(int i=2;i<arr.length;i++){
+        //     if(arr[i]-arr[i-1]!=diff)
+        //     return false;
+            
+        // }
+        // return true;
 
-        for(int i = 2; i < arr.length; i++) {
-            if(arr[i] - arr[i - 1] != diff) {
-                return false;
+        int len = arr.length;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num < min) {
+                min = num;
+            }
+
+            if (num > max) {
+                max = num;
             }
         }
+
+        if (max == min) {
+            return true;
+        }
+
+        int diff = max - min;
+
+        if (diff % (len - 1) != 0) {
+            return false;
+        }
+
+        int step = diff / (len - 1);
+
+        boolean[] seen = new boolean[len];
+
+        for (int num : arr) {
+            int pos = (num - min) / step;
+
+            if ((num - min) % step != 0) {
+                return false;
+            }
+
+            if (seen[pos]) {
+                return false;
+            }
+            
+            seen[pos] = true;
+        }
+
         return true;
     }
 }
