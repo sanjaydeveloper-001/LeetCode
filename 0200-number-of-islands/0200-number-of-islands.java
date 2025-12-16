34118de5580bd661aa0afaed4a2e1643eb025a5e
Sanjay D <josephstudent001@gmail.com>
2025-12-16 09:42:38 +0530

    Time: 2 ms (99.87%), Space: 52.4 MB (35.34%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -3,30 +3,31 @@ class Solution {
         int r = grid.length;
         int c = grid[0].length;
 
-        int iland = 0;
-
+        int count = 0;
         for(int i=0; i<r; i++){
             for(int j=0; j<c; j++){
-                if(grid[i][j] == '1'){
-                    iland++;
-                    changetoWater(grid,i,j,r,c);
+                if(grid[i][j] == '1'){ 
+                    funt(grid, i, j);
+                    count++;
                 }
             }
         }
-
-        return iland;
+        return count;
 
     }
 
-    public void changetoWater( char[][] grid, int i, int j, int r, int c){
-        if(i<0 || j<0 || i>=r || j>=c || grid[i][j] == '0' ){
-            return;
-        }
-        grid[i][j] = '0';
 
-        changetoWater(grid , i-1, j,r,c);
-        changetoWater(grid , i+1, j,r,c);
-        changetoWater(grid , i, j-1,r,c);
-        changetoWater(grid , i, j+1,r,c);
+    public static void funt(char [][]grid, int i, int j){
+        int r = grid.length;
+        int c = grid[0].length;
+
+        if( i < 0 || j < 0 || i >= r || j >= c || grid[i][j] == '0') return;
+        grid[i][j] = '0';
+        
+        funt(grid, i+1, j); //Down
+        funt(grid, i, j+1); //Right
+        funt(grid, i-1, j); //Up
+        funt(grid, i, j-1); //Left
+
     }
 }
