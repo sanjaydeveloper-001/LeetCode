d09f4a0b3e101d26af687b8fc31aa6e9515c6d65
Sanjay D <josephstudent001@gmail.com>
2025-12-30 18:52:26 +0530

    Time: 0 ms (100%), Space: 43.6 MB (17.05%) - LeetHub

--- 0840-magic-squares-in-grid/0840-magic-squares-in-grid.java
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.java
@@ -1,35 +1,46 @@
 class Solution {
-    public boolean isMagicSquare(int[][] grid,int r,int c){
-        int sum = grid[r][c] + grid[r][c+1] + grid[r][c+2];
-        HashSet<Integer> set = new HashSet<>();
- 
-        for(int i=0;i<3;i++){ // 1 - 9 checking
-            for(int j=0;j<3;j++){
-                int num = grid[r+i][c+j];
-                if(num<1 || num>9 || set.contains(num)) return false;
-                set.add(num);
-            }
-        }
-        
-        for(int i=0;i<3;i++){ // row and col sum checking
-            if((grid[r][c+i] + grid[r+1][c+i] + grid[r+2][c+i]) != sum) return false;
-            if(grid[r+i][c] + grid[r+i][c+1] + grid[r+i][c+2] != sum) return false;
-        }
-
-        //diagonal sum checking
-        if((grid[r][c] + grid[r+1][c+1] + grid[r+2][c+2]) != sum) return false;
-        if(grid[r][c+2] + grid[r+1][c+1] + grid[r+2][c] != sum) return false;
-        return true;
-    }
-
     public int numMagicSquaresInside(int[][] grid) {
-        int m = grid.length,n = grid[0].length;
-        int c = 0;
-        for(int i=0;i<=m-3;i++){
-            for(int j=0;j<=n-3;j++){
-                if(isMagicSquare(grid,i,j)) c++;
+        int n = grid.length;
+        int m = grid[0].length;
+        if (n < 3 || m < 3) {
+            return 0;
+        }
+        int count = 0;
+        for (int i = 0; i <= n - 3; i++) {
+            for (int j = 0; j <= m - 3; j++) {
+                if (isMagic(grid, i, j)) {
+                    count++;
+                }
             }
         }
-        return c;
+        return count;
+    }
+    private boolean isMagic(int[][] grid, int r, int c) {
+        boolean[] seen = new boolean[10];
+        for (int i = r; i < r + 3; i++) {
+            for (int j = c; j < c + 3; j++) {
+                int num = grid[i][j];
+                if (num < 1 || num > 9 || seen[num]) {
+                    return false;
+                }
+                seen[num] = true;
+            }
+        }
+        int sum = grid[r][c] + grid[r][c+1] + grid[r][c+2];
+        for (int i = 0; i < 3; i++) {
+            if (grid[r+i][c] + grid[r+i][c+1] + grid[r+i][c+2] != sum) {
+                return false;
+            }
+            if (grid[r][c+i] + grid[r+1][c+i] + grid[r+2][c+i] != sum) {
+                return false;
+            }
+        }
+        if (grid[r][c] + grid[r+1][c+1] + grid[r+2][c+2] != sum) {
+            return false;
+        }
+        if (grid[r][c+2] + grid[r+1][c+1] + grid[r+2][c] != sum) {
+            return false;
+        }
+        return true;
     }
 }
