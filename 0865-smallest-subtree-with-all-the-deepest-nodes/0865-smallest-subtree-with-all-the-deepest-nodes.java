207d04ec645dcad98cb5fbb7cbcdc32504d3f979
Sanjay D <josephstudent001@gmail.com>
2026-01-09 20:36:06 +0530

    Time: 0 ms (100%), Space: 43.7 MB (18.16%) - LeetHub

--- 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.java
+++ 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.java
@@ -1,45 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
+
     public TreeNode subtreeWithAllDeepest(TreeNode root) {
-        if (root == null) return null;
+        return dfs(root).node;
+    }
 
-        Map<TreeNode, TreeNode> parent = new HashMap<>();
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-        parent.put(root, null);
-
-        List<TreeNode> lastLevel = new ArrayList<>();
-
-        // BFS traversal
-        while (!q.isEmpty()) {
-            int size = q.size();
-            lastLevel.clear();
-
-            for (int i = 0; i < size; i++) {
-                TreeNode node = q.poll();
-                lastLevel.add(node);
-
-                if (node.left != null) {
-                    parent.put(node.left, node);
-                    q.offer(node.left);
-                }
-                if (node.right != null) {
-                    parent.put(node.right, node);
-                    q.offer(node.right);
-                }
-            }
+    private Result dfs(TreeNode root) {
+        if (root == null) {
+            return new Result(null, 0);
         }
 
-        Set<TreeNode> deepest = new HashSet<>(lastLevel);
+        Result left = dfs(root.left);
+        Result right = dfs(root.right);
 
-        // Move upward until all nodes converge
-        while (deepest.size() > 1) {
-            Set<TreeNode> next = new HashSet<>();
-            for (TreeNode node : deepest) {
-                next.add(parent.get(node));
-            }
-            deepest = next;
+        if (left.depth > right.depth) {
+            return new Result(left.node, left.depth + 1);
+        } else if (right.depth > left.depth) {
+            return new Result(right.node, right.depth + 1);
+        } else {
+            return new Result(root, left.depth + 1);
         }
+    }
 
-        return deepest.iterator().next();
+    class Result {
+        TreeNode node;
+        int depth;
+
+        Result(TreeNode node, int depth) {
+            this.node = node;
+            this.depth = depth;
+        }
     }
 }
