8a59e98c4fb37d4e95c94c23e84122dc87292711
Sanjay D <23it1249@stjosephstechnology.ac.in>
2026-03-16 10:01:19 +0530

    Time: 0 ms (100%), Space: 46.6 MB (34.85%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -4,13 +4,11 @@ class Solution {
         int min = arr[0];
 
         for(int i=1; i<arr.length; i++){
-            int c_p = 0;
-            if(min > arr[i]) min =arr[i]; 
-            else if(min < arr[i]) {
+            int c_p = 0; 
+            if(min < arr[i]) {
                 c_p = arr[i]-min;
-                min = arr[i];
             }
-
+            min = arr[i];
             p=Math.max(p+c_p, c_p);
         }
 
