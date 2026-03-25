532f795d90dbfa3f02bfb7d49aebf2c6d85f2663
Sanjay D <23it1249@stjosephstechnology.ac.in>
2026-03-25 08:45:33 +0530

    Time: 3 ms (42.71%), Space: 46.8 MB (38.54%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -3,35 +3,29 @@ class Solution {
     public List<List<String>> answer = new ArrayList<>();
     
     public boolean isSafe(char[][] board, int row, int col){
-       
         for(int i=row; i>=0; i--){
             if(board[i][col] == 'Q'){
                 return false;
             }
         }
-        
         for(int i = row-1, j = col-1; i>=0 && j>=0; i-- , j--){
             if(board[i][j] == 'Q'){
                 return false;
             }
         }
-        
         for(int i=row-1, j=col+1; i>=0 && j<board.length; i--, j++){
             if(board[i][j] == 'Q'){
                 return false;
             }
         }
-
         return true;
     }
 
     public void addInList(char[][] board){
-        List<String> str = new ArrayList<>();
-               
+        List<String> str = new ArrayList<>();      
         for(char[] row:board){
             str.add(new String(row));
         }
-
         answer.add(str); 
     }
 
