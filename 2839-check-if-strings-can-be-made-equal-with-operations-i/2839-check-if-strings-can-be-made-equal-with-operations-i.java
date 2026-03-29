9acb3990a2ebe0744aaee5400686a42855863e72
Sanjay D <23it1249@stjosephstechnology.ac.in>
2026-03-29 12:46:50 +0530

    Time: 1 ms (100%), Space: 44.7 MB (11.22%) - LeetHub

--- 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.java
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.java
@@ -1,24 +1,8 @@
 class Solution {
     public boolean canBeEqual(String s1, String s2) {
-        if(s1.equals(s2)) return true;
-        String s11 = s1;
-        int n = s1.length();
-        char []ch = s1.toCharArray();
-        char []c2 = s11.toCharArray();
-        for(int i=0; i<n-2; i++){
-            char t = ch[i];
-            ch[i] = ch[i+2];
-            ch[i+2] = t;
-            if((new String(ch).equals(s2))) return true;
-        }
-
-        for(int i=n-1; i>1; i--){
-            char t = c2[i];
-            c2[i] = c2[i-2];
-            c2[i-2] = t;
-            if((new String(c2).equals(s2))) return true;
-        }
-
-        return false;
+        return ((s1.charAt(0) == s2.charAt(0) && s1.charAt(2) == s2.charAt(2)) ||
+                (s1.charAt(0) == s2.charAt(2) && s1.charAt(2) == s2.charAt(0))) &&
+               ((s1.charAt(1) == s2.charAt(1) && s1.charAt(3) == s2.charAt(3)) ||
+                (s1.charAt(1) == s2.charAt(3) && s1.charAt(3) == s2.charAt(1)));
     }
 }
